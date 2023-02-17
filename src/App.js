efa6510fee9361419e62b2db518af8214c466f6d
basha14322 <125136744+basha14322@users.noreply.github.com>
2023-02-17 12:59:49 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="ethics">
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
